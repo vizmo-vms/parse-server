3d051b61a0e2d64354cf6ad967367ce37103f83d
Rahul Lanjewar <rahullanjewar93@gmail.com>
2025-05-26 09:35:54 +0530

    fix: og express handler

--- src/middlewares.js
+++ src/middlewares.js
@@ -463,9 +463,6 @@ export function allowMethodOverride(req, res, next) {
 export function handleParseErrors(err, req, res, next) {
   const log = (req.config && req.config.loggerController) || defaultLogger;
   if (err instanceof Parse.Error) {
-    if (req.config && req.config.enableExpressErrorHandler) {
-      return next(err);
-    }
     let httpStatus;
     // TODO: fill out this mapping
     switch (err.code) {
@@ -481,6 +478,9 @@ export function handleParseErrors(err, req, res, next) {
     res.status(httpStatus);
     res.json({ code: err.code, error: err.message });
     log.error('Parse error: ', err);
+    if (req.config && req.config.enableExpressErrorHandler) {
+      return next(err);
+    }
   } else if (err.status && err.message) {
     res.status(err.status);
     res.json({ error: err.message });
