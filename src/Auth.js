1e312cdf201d226e9d98c63ad79eeae86509884e
Nikhil <nikhil@vizmo.in>
2025-06-13 23:06:06 +0530

    fix: review fixes

--- src/Auth.js
+++ src/Auth.js
@@ -449,11 +449,15 @@ const findUsersWithAuthData = async (config, authData, beforeFind) => {
 };
 
 const hasMutatedAuthData = (authData, userAuthData) => {
-  if (!userAuthData) { return { hasMutatedAuthData: true, mutatedAuthData: authData }; }
+  if (!userAuthData) {
+    return { hasMutatedAuthData: true, mutatedAuthData: authData };
+  }
   const mutatedAuthData = {};
   Object.keys(authData).forEach(provider => {
     // Anonymous provider is not handled this way
-    if (provider === 'anonymous') { return; }
+    if (provider === 'anonymous') {
+      return;
+    }
     const providerData = authData[provider];
     const userProviderAuthData = userAuthData[provider];
     if (!isDeepStrictEqual(providerData, userProviderAuthData)) {
@@ -511,13 +515,11 @@ const checkIfUserHasProvidedConfiguredProvidersForLogin = (
     return;
   }
 
-  let errorMsg  =  `Missing additional authData ${additionProvidersNotFound.join(',')}`
-  if(additionProvidersNotFound.length>0){
+  let errorMsg = `Missing additional authData ${additionProvidersNotFound.join(',')}`;
+  if (additionProvidersNotFound.length > 0) {
     const mfaData = userAuthData.mfa || {};
-    const needsToken = !mfaData.secret && (mfaData.email || mfaData.mobile);
-    const additionalInfo = needsToken
-      ? " (include 'request' as token to receive a verification code)"
-      : "";
+    const needsToken = !mfaData.secret;
+    const additionalInfo = needsToken ? ' Send request for additional auth data' : '';
 
     errorMsg += additionalInfo;
   }
