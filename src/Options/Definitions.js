9059f5db9ca7e51e0da160d6b7d3188889477f4a
Hari Prasad <hariprasadiit@gmail.com>
2026-02-15 20:07:09 +0530

    fix: remove keepUnknownIndexes and standardize dropUnknownIndexes

--- src/Options/Definitions.js
+++ src/Options/Definitions.js
@@ -28,12 +28,12 @@ module.exports.SchemaOptions = {
     action: parsers.booleanParser,
     default: false,
   },
-  keepUnknownIndexes: {
-    env: 'PARSE_SERVER_SCHEMA_KEEP_UNKNOWN_INDEXES',
+  dropUnknownIndexes: {
+    env: 'PARSE_SERVER_SCHEMA_DROP_UNKNOWN_INDEXES',
     help:
-      'Keep indexes that are not defined in the schema and are present in the database. Set this to true if you are adding indexes manually so that it wont be dropped when you run schema migration',
+      "Drops indexes that are not defined in the schema and are present in the database. Set this to `false` if you are adding indexes manually so that they won't be dropped when running schema migration.",
     action: parsers.booleanParser,
-    default: false,
+    default: true,
   },
   lockSchemas: {
     env: 'PARSE_SERVER_SCHEMA_LOCK_SCHEMAS',
--- src/SchemaMigrations/Migrations.js
+++ src/SchemaMigrations/Migrations.js
@@ -6,7 +6,7 @@ export interface SchemaOptions {
   deleteExtraFields: ?boolean;
   recreateModifiedFields: ?boolean;
   lockSchemas: ?boolean;
-  keepUnknownIndexes: ?boolean;
+  dropUnknownIndexes: ?boolean;
   beforeMigration: ?() => void | Promise<void>;
   afterMigration: ?() => void | Promise<void>;
 }
